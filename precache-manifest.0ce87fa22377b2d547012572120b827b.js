b982ef72b25070f1911a99235bb6b3cef1ff2de4
Krishna Vikas <venkatakrishnavika.b@husky.neu.edu>
2020-06-24 12:57:14 -0400

    Updates

---
+++ precache-manifest.0ce87fa22377b2d547012572120b827b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "079c7b817ef7a07df27a9d7b65194ffa",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "0c852db518b18973d53c",
+    "url": "/weather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a12dafc71bb38f3c70a7",
+    "url": "/weather/static/js/2.ac1a06f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.ac1a06f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c852db518b18973d53c",
+    "url": "/weather/static/js/main.d81a6355.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
